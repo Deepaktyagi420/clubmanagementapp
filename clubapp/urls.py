d3eb4184acdfe1fa8952545af1ea07fb61f2d668
Deepak <97d.tyagi@gmail.com>
2024-04-07 18:19:58 +0530

    final
    Final Build

---
+++ clubapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path('login/', views.UserLoginView.as_view(), name='login_page'),
+    path('logout/', views.LogoutView.as_view(), name='logout_page'),
+    path('register/', views.UserRegisterView.as_view(), name='user_register'),
+    path('home/', views.HomeView.as_view(), name="home_page"),
+    path('about-us/', views.AboutUsView.as_view(), name="about_us"),
+    path('activity-list/', views.ActivitiesListView.as_view(), name="activity_list"),
+    path('activity/<int:pk>', views.ActivityDetailView.as_view(), name="activity_detail"),
+    path('my-booking/', views.MyBookingsView.as_view(), name="my_booking_view"),
+]
---
+++ clubapp/views.py
@@ -0,0 +1,87 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views import View
+from django.urls import reverse
+from django.contrib.auth.models import User
+from clubapp.models import ActivitiesModel, BookedActivity
+from django.contrib.auth import logout
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('login_page')
+
+class UserLoginView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, template_name="login.html", context={})
+    
+    def post(self, request, *args, **kwargs):
+        username = request.POST["username"]
+        password = request.POST["password_id"]
+        user = User.objects.filter(username=username, password=password).first()
+        if user:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect("home_page")
+        else:
+            # Return an 'invalid login' error message.
+            return render(request, template_name="login.html", context={"error":"Invalid credentials"})
+        
+
+class UserRegisterView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, template_name="register.html", context={})
+    
+    def post(self, request, *args, **kwargs):
+        if request.POST.get("password") != request.POST.get("checkpassword"):
+            return render(request, template_name="register.html", context={"error":"password does not match"})
+        
+        if User.objects.filter(username=request.POST.get("username")).exists():
+            return render(request, template_name="register.html", context={"error":"user already exists"})
+        
+        User.objects.create(username=request.POST.get("username"), email=request.POST.get("email"), first_name=request.POST.get("name").split(" ")[0], last_name=request.POST.get("name").split(" ")[1], password=request.POST.get("password"))
+
+        return redirect("login_page")
+
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, template_name="home.html", context={})
+
+class AboutUsView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, template_name="aboutus.html", context={})
+    
+class ActivitiesListView(View):
+    def get(self, request, *args, **kwargs):
+        activity_lst = ActivitiesModel.objects.all()
+        context = {"activity_list":activity_lst}
+        return render(request, template_name="activities_lst.html", context=context)
+
+class ActivityDetailView(View):
+    error_msg = ""
+    context = {}
+    def get(self, request, pk, *args, **kwargs):
+        activity_details = ActivitiesModel.objects.filter(id=pk).values().first()
+        return render(request, template_name="activity_detail.html", context={"detail":activity_details})
+
+    def post(self, request, pk, *args, **kwargs):
+        activity = ActivitiesModel.objects.filter(id=pk).first()
+        try:
+            BookedActivity.objects.create(user=request.user, activity=activity, activity_slot=request.POST.get("time_frame"), date_of_booking=request.POST.get("date"))
+        except Exception as e:
+            tt="TT"
+
+        url = reverse('activity_detail', kwargs={'pk':pk})
+        return redirect(url)
+    
+class MyBookingsView(View):
+    def get(self, request, pk=None, *args, **kwargs):
+        activties = BookedActivity.objects.filter(user=request.user)
+        return render(request, template_name="mybookings.html", context={"booking_lst":activties})
