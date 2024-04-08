50435a65e147563202bc2b35f9efcc1c41a716fe
Deepak <166137085+Deepaktyagi420@users.noreply.github.com>
2024-04-08 20:45:00 +0000

    changes

--- clubapp/urls.py
+++ clubapp/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('login/', views.UserLoginView.as_view(), name='login_page'),
     path('logout/', views.LogoutView.as_view(), name='logout_page'),
     path('register/', views.UserRegisterView.as_view(), name='user_register'),
+    path('user-details/', views.UpdateUserDetailsView.as_view(), name='user_details'),
+    path('booking-details/<int:pk>', views.BookingDetailView.as_view(), name='booking_details'),
     path('', views.HomeView.as_view(), name="home_page"),
     path('about-us/', views.AboutUsView.as_view(), name="about_us"),
     path('activity-list/', views.ActivitiesListView.as_view(), name="activity_list"),
